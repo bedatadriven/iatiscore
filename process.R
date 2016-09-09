dae7410b77916aa2c2e16efc9703c645b8563cd0
Alex Bertram <alex@bedatadriven.com>
2016-09-09 15:41:56 +0200

    Real data, crappy vizualization

--- process.R
+++ process.R
@@ -20,22 +20,23 @@ for(i in seq_along(results)) {
   extras <- result$extras
   names(extras) <- sapply(result$extras, function(e) e$key)
   
-  pub[i, "name"] <- nz(result$organization$name)
+  pub[i, "name"] <- nz(result$title)
   pub[i, "iati_id"] <- nz(extras$publisher_iati_id$value)
   pub[i, "country"] <- nz(extras$publisher_country$value)
   pub[i, "source_type"] <- nz(extras$publisher_source_type$value)
   pub[i, "org_type"] <- nz(extras$publisher_organization_type$value)
-  pub[i, "score"] <- runif(1, min=0, max=15)
 }
 
 # Remove duplicates
-pub <- pub[!duplicated(pub$iati_id) & !is.na(pub$name), ]
+pub <- pub[!duplicated(pub$iati_id), ]
 
+# Remove those without names
+pub <- pub[!is.na(pub$name), ]
+pub$name <- gsub(pub$name, pattern = "\\s*organi[sz]ation file", replacement = "", ignore.case = TRUE)
 
 ### LOAD SCORES
 
-
-scores <- read.table("iati/scores.tsv", stringsAsFactors = FALSE, sep = "\t")
+scores <-readr::read_delim("iati/scores.tsv", delim="\t")
 names(scores) <- c("iati_id", "activity_id", "score1", "ref_score", "tx_eur", "tx_count")
 
 pubscore <- aggregate(ref_score ~ iati_id, data = scores, FUN = mean, na.action = na.omit)
@@ -45,14 +46,12 @@ pubscore <- aggregate(ref_score ~ iati_id, data = scores, FUN = mean, na.action 
 
 sumtab <- merge(pub, pubscore, all.x = TRUE)
 
-
 countries <- sort(unique(pub$country))
 
 for(country in countries) {
   cat(sprintf("%s\n", country))
-  country.scores <- pub[pub$country == country, c("name", "iati_id", "score")]
-  country.scores <- country.scores[ order(-country.scores$score), ]
-  
+  country.scores <- sumtab[sumtab$country == country, ]
+
   write.csv(file = sprintf("data/%s.csv", country), 
             row.names = FALSE,
             country.scores)
